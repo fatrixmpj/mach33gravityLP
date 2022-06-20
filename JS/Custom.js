2f6591a46ca9eb8401376b11d41f6527507de943
Patrick <fatrixmpj@hotmail.com>
2022-06-20 21:24:46 +0200

    gsap anims

--- JS/Custom.js
+++ JS/Custom.js
@@ -38,3 +38,59 @@ function toggleWhiteMode() {
     whiteBtn.classList.remove("btn-outline-dark");
   }
 }
+
+// TABS
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
+// GSAP ANIMATIONS
+gsap.defaults({ease: "none", duration: 2});
+
+let scrollTl = gsap.timeline();
+
+scrollTl.to(".section1", {
+    // xPercent: -100,
+    opacity: 0
+}).to(".section2", {
+    // xPercent: -100,
+    opacity: 0
+}).to(".section3", {
+    // xPercent: -100,
+    opacity: 0
+});
+
+ScrollTrigger.create({
+    animation: scrollTl,
+    trigger: ".main",
+    start: "top top",
+    end: "+=4000", 
+    scrub: true,
+    pin: true,
+    anticipatePin: 1,
+    markers: true
+  });
+
+// GSDevTools.create();to
